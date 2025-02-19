dfcae52338fa96e4e3d4b7b6f28db2bd5595fc1b
Ruben Leis <rubenleisfp@gmail.com>
2025-02-19 09:09:22 +0100

    Agregamos metodo para buscar

--- src/main/java/com/castelaofp/biblioteca/repository/LibroRepository.java
+++ src/main/java/com/castelaofp/biblioteca/repository/LibroRepository.java
@@ -3,7 +3,14 @@ package com.castelaofp.biblioteca.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.castelaofp.biblioteca.model.Libro;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LibroRepository extends JpaRepository<Libro, Long> {
-}
 
+	@Query("SELECT l FROM Libro l WHERE l.titulo LIKE %:q% OR l.autor LIKE %:q%")
+	List<Libro> findByAutorContainingOrTituloContaining(@Param("q") String q);
+
+}
